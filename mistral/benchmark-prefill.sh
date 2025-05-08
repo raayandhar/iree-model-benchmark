d7bc8e9cab08b36040d865e2cb0981853c450419
MaheshRavishankar <1663364+MaheshRavishankar@users.noreply.github.com>
2025-05-07 22:59:34 -0700

    Add new Mistral model benchmarks (fp8 version). (#8)
    Signed-off-by: MaheshRavishankar <mravisha@amd.com>

---
+++ mistral/benchmark-prefill.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Usage: PATH=/path/to/iree/build/tools:$PATH ./benchmark-unet.sh N
+
+set -euo pipefail
+
+if (( $# != 1 && $# != 2 )); then
+  echo "usage: $0 <hip-device-id> [<irpa-path>]"
+  exit 1
+fi
+
+readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+readonly WORKING_DIR="${WORKING_DIR:-${SCRIPT_DIR}/tmp}"
+readonly PREFIX="${PREFIX:-base}"
+readonly IREE_BENCHMARK="$(which iree-benchmark-module)"
+readonly HIP_DEVICE="$1"
+readonly USE_TRACY="${USE_TRACY:-0}"
+readonly IREE_TRACY_CAPTURE="$(which iree-tracy-capture)"
+
+readonly -a INPUTS=(
+  "--input=16x1024xsi64"
+  "--input=16xsi64"
+  "--input=16x32xsi64"
+  "--input=2048x2621440xf8E4M3FNUZ"
+)
+
+# IRPA file:
+# Size: 16061181952
+# md5sum: 8f4685d6799298609152dd509ba32e88
+readonly IRPA="${2:-/data/Mistral-Nemo-Instruct-2407-FP8/quark_mistral_nemo.irpa}"
+
+echo "Using IRPA file:"
+stat -c "%y %s %n" "${IRPA}"
+# md5sum "${IRPA}"
+
+set -x
+
+run_benchmark() {
+    "$IREE_BENCHMARK" \
+	--device="hip://${HIP_DEVICE}" \
+	--device_allocator=caching \
+	--hip_use_streams=true \
+	--module="${WORKING_DIR}/${PREFIX}.vmfb" \
+	--parameters=model="${IRPA}" \
+	--function=prefill_bs16 \
+	"${INPUTS[@]}" \
+	--benchmark_repetitions=3
+}
+
+if (( "${USE_TRACY}" == "1")); then
+    TRACY_PORT=8087 IREE_PY_RUNTIME=tracy TRACY_NO_EXIT=1 run_benchmark &
+    "${IREE_TRACY_CAPTURE}" -f -p 8087 -o "${WORKING_DIR}/${PREFIX}.tracy"
+else
+    run_benchmark
+fi
---
+++ mistral/compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Usage: PATH=/path/to/iree/build/tools:$PATH ./compile-fp8.sh <target-chip> [extra flags]
+
+set -euo pipefail
+
+if (( $# < 1 )); then
+  echo "usage: $0 <hip-target-chip>"
+  exit 1
+fi
+
+readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+readonly IREE_COMPILE="$(which iree-compile)"
+readonly CHIP="$1"
+shift 1
+
+readonly WORKING_DIR="${WORKING_DIR:-${SCRIPT_DIR}/tmp}"
+readonly PREFIX="${PREFIX:-base}"
+
+set -x
+
+"${SCRIPT_DIR}/compile-base.sh" "$IREE_COMPILE" "$CHIP" \
+  "${SCRIPT_DIR}/base_ir/quark_mistral_nemo_16x128.mlir" \
+  -o "${WORKING_DIR}/${PREFIX}.vmfb" \
+  "$@"
