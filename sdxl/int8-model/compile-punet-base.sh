fba7e1401a671508e9acbb541b55507435b73d55
raayandhar <rdhar@amd.com>
2025-09-26 19:27:45 +0000

    add optional spec file

--- sdxl/int8-model/compile-punet-base.sh
+++ sdxl/int8-model/compile-punet-base.sh
@@ -2,7 +2,7 @@
 
 # Base unet compilation script. This is intended to be invoked by other scripts.
 # Usage:
-# ./compile-unet-base.sh <iree-compile-path> <gfxip> <input mlir> -o <output vmfb> [extra flags]
+# ./compile-unet-base.sh <iree-compile-path> <gfxip> <input mlir> [spec_file] -o <output vmfb> [extra flags]
 
 set -euo pipefail
 
@@ -22,7 +22,17 @@ if [ ! -f "$INPUT" ] ; then
   exit 1
 fi
 
-shift 3
+SPEC_FILE=""
+if [ $# -gt 3 ] && [[ "$4" != -* ]]; then
+  SPEC_FILE="$(realpath "$4")"
+  if [ ! -f "$SPEC_FILE" ] ; then
+    echo "Specified spec file not found: ${SPEC_FILE}"
+    exit 1
+  fi
+  shift 4
+else
+  shift 3
+fi
 
 readonly FLAGS=(
 "--iree-preprocessing-pass-pipeline=builtin.module(util.func(iree-global-opt-raise-special-ops, iree-flow-canonicalize), iree-preprocessing-transpose-convolution-pipeline, iree-preprocessing-pad-to-intrinsics{pad-target-type=conv}, util.func(iree-preprocessing-generalize-linalg-matmul-experimental))"
@@ -30,13 +40,23 @@ readonly FLAGS=(
 
 set -x
 
-"$IREE_COMPILE" "$INPUT" \
-    --iree-hal-target-backends=rocm \
-    --iree-hip-target=$CHIP \
-    --iree-execution-model=async-external \
-    --iree-opt-level=O3 \
-    --iree-vm-target-truncate-unsupported-floats \
-    --iree-codegen-llvmgpu-use-vector-distribution \
-    --iree-execution-model=async-external \
-    "${FLAGS[@]}" \
-    "$@"
+CMD_ARGS=(
+    "$IREE_COMPILE" "$INPUT"
+    --iree-hal-target-backends=rocm
+    --iree-hip-target=$CHIP
+    --iree-execution-model=async-external
+    --iree-opt-level=O3
+    --iree-vm-target-truncate-unsupported-floats
+    --iree-codegen-llvmgpu-use-vector-distribution
+    --iree-llvmgpu-enable-prefetch=1
+    --iree-config-add-tuner-attributes
+    "${FLAGS[@]}"
+)
+
+if [ -n "$SPEC_FILE" ]; then
+    CMD_ARGS+=(--iree-codegen-transform-dialect-library="$SPEC_FILE")
+fi
+
+CMD_ARGS+=("$@")
+
+"${CMD_ARGS[@]}"
